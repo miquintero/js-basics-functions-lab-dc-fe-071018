f6e773ec516ae806c221fb8dac1e1a865b1caf54
Alexa Schaeffer <alps231@gmail.com>
2018-08-19 21:24:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function distanceFromHqInBlocks (blockNumber) {
   if (blockNumber > 42) {
-    return blockNumber - 42; 
+    return blockNumber - 42;
   } else {
     return 42 - blockNumber;
   }
+}
+
+function distanceFromHqInFeet (blockNumber) {
+  return distanceFromHqInBlocks (blockNumber) * 264;
 }
 
 function distanceTravelledInFeet (beginningBlock, destinationBlock) {
