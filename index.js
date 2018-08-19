78c686c87c21a84d135abfd0454f3dd3d91ff93b
Alexa Schaeffer <alps231@gmail.com>
2018-08-19 21:12:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function distanceFromHqInBlocks ( ) {
-  
+function distanceFromHqInBlocks (blocks) {
+let blocks = 42 - pickUpLocation
+  if (blocks = 42) {
+    return "0 blocks from HQ"
+  } else if (42 - blocks) {
+    return ""
+  }
+
 }
